82e3a43c30ce977597fc8b192e8255de644f7084
汤佳杰 <mayday@I.local>
2019-04-08 15:31:57 +0800

    update

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ make an interface for a bunch of related classes and many of their derived class
 ### builder
 
 separate a complex object's construction with its representation.
+
+### prototype
+
+changes values passed in to get new objects without defining many derived classes.
+
+### singleton
+
+only allows user to instance one object for one class.
---
+++ singleton.cpp
@@ -0,0 +1,148 @@
+#include<algorithm>
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<string>
+using namespace std;
+class Spell {
+public:
+    Spell() {
+        int n = rand()%10;
+        for (int i = 0; i < n; ++i) {
+            curse.insert(curse.end(), static_cast<char>('0' + rand()%10));
+        }
+    }
+private:
+    string curse;
+};
+enum Direction {
+    EAST,
+    WEST,
+    NORTH,
+    SOUTH
+};
+class MapSite {
+public:
+    virtual void Enter() = 0;
+};
+class Room : public MapSite {
+public:
+    Room(int roomNo){
+        _roomNumber = roomNo;
+    }
+    MapSite* GetSite(Direction d) const {
+        return _sides[d];
+    }
+    void SetSide(Direction d, MapSite* m){
+        _sides[d] = m;
+    }
+    virtual void Enter() override {}
+protected:
+    MapSite* _sides[4];
+    int _roomNumber;
+};
+class EnchantedRoom : public Room {
+public:
+    EnchantedRoom(int roomNo, Spell* s) : Room(roomNo), _spell(s){}
+protected:
+    Spell* _spell;
+};
+class Wall : public MapSite {
+public:
+    Wall(){}
+    virtual void Enter() override {}
+};
+class Door : public MapSite {
+public:
+    Door(Room* r1 = 0, Room* r2 = 0){
+        _room1 = r1;
+        _room2 = r2;
+    }
+    virtual void Enter() override {}
+    Room* OtherSideFrom(Room* r){
+        if (r == _room2) return _room1;
+        else return _room2;
+    }
+
+protected:
+    Room* _room1;
+    Room* _room2;
+    bool isOpen;
+};
+class DoorNeedingSpell : public Door {
+public:
+    DoorNeedingSpell(Room* r1, Room* r2) : Door(r1,r2){}
+};
+class Maze {
+public:
+    Maze(){
+        _rooms.clear();
+    }
+    void AddRoom(Room* r){
+        _rooms.push_back(r);
+    }
+    Room* RoomNo(int idx) const{
+        if (idx >= _rooms.size()) return nullptr;
+        return _rooms[idx-1];
+    }
+
+private:
+    vector<Room*> _rooms;
+};
+class MazeFactory {
+public:
+    static MazeFactory* Instance() {
+        if (_instance == 0) {
+            _instance = new MazeFactory;
+        }
+        return _instance;
+    }
+    virtual Maze* MakeMaze() const {
+        return new Maze;
+    }
+    virtual Wall* MakeWall() const {
+        return new Wall;
+    }
+    virtual Room* MakeRoom(int n) const {
+        return new Room(n);
+    }
+    virtual Door* MakeDoor(Room* r1, Room* r2) const {
+        return new Door(r1, r2);
+    }
+
+protected:
+    MazeFactory(){}
+private:
+    static MazeFactory* _instance;
+};
+MazeFactory* MazeFactory::_instance = 0;
+
+class MazeGame {
+public:
+    Maze* CreateMaze(MazeFactory& factory) {
+        Maze* aMaze = factory.MakeMaze();
+        Room* r1 = factory.MakeRoom(1);
+        Room* r2 = factory.MakeRoom(2);
+        Door* aDoor = factory.MakeDoor(r1,r2);
+        aMaze->AddRoom(r1);
+        aMaze->AddRoom(r2);
+        r1->SetSide(NORTH, factory.MakeWall());
+        r1->SetSide(EAST, aDoor);
+        r1->SetSide(SOUTH, factory.MakeWall());
+        r1->SetSide(WEST, factory.MakeWall());
+
+        r2->SetSide(NORTH, factory.MakeWall());
+        r2->SetSide(EAST, factory.MakeWall());
+        r2->SetSide(SOUTH, factory.MakeWall());
+        r2->SetSide(WEST, aDoor);
+
+        return aMaze;
+    }
+
+};
+
+int main() {
+    MazeFactory* factory = MazeFactory::Instance();
+    MazeGame mg;
+    Maze* m = mg.CreateMaze(*factory);
+}
